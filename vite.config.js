f3fe31b133642d87ade5562fc1d31dd130dca0aa
b1mind <brent@1mindsolutions.com>
2024-10-04 13:20:25 -0600

    config: remove unused lockfile, use current sass in vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		fs: {
 			// Allow access to files from the project root.
-			allow: ['..']
-		}
-	}
-});
+			allow: ['..'],
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				api: 'modern',
+			},
+		},
+	},
+})
