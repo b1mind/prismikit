d2af75c2f2879ab0cc7810103ca4dc5c8f5eb0fa
b1mind <brent@1mindsolutions.com>
2024-10-06 00:58:30 -0600

    fix: date for posts from first publish

--- src/routes/[[preview=preview]]/blog/[uid]/+page.svelte
+++ src/routes/[[preview=preview]]/blog/[uid]/+page.svelte
@@ -13,17 +13,20 @@
 			<span>{tag}</span>
 		{/each}
 	</div>
-	<i>{data.page.data.date}</i>
+	<i>{new Date(data.page.first_publication_date).toDateString()}</i>
 
 	<SliceZone slices={data.slices} {components} />
 
 	<!-- todo make related default of page? -->
+	<h2>Related:</h2>
 	{#each data.related as post}
 		<li>
 			<a href={post.url}>
 				{post.data.title}
 			</a>
 		</li>
+	{:else}
+		<p>no related posts</p>
 	{/each}
 </article>
 
